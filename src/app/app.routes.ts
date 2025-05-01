ce7f3b12a7c7cd48807714ac796c90d01735d97f
larapodjavrsek <lara.podjavrsek@gmail.com>
2025-05-01 23:12:39 +0200

    dodano navigiranje

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { DomovComponent } from './domov/domov.component';
+import { DodajComponent } from './dodaj/dodaj.component';
+import { PrijavaComponent } from './prijava/prijava.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DomovComponent},
+    {path: 'dodaj', component: DodajComponent},
+    {path: 'prijava', component: PrijavaComponent}
+];
+
+
---
+++ src/app/domov/domov.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-domov',
+  imports: [],
+  templateUrl: './domov.component.html',
+  styleUrl: './domov.component.css'
+})
+export class DomovComponent {
+
+}
